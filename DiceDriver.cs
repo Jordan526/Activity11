e9e23abd4530bd16b3f5372af2b9f76a0768abe8
Jordan526 <96961388+Jordan526@users.noreply.github.com>
2022-06-20 21:49:40 -0700

    Add files via upload

---
+++ DiceDriver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity11
+{
+    public class DiceDriver
+    {
+        public static void Main()
+        {
+
+            //initialize Dice class and pass in valid int number. 
+            Dice dice1 = new Dice(9);
+            Dice dice2 = new Dice(6);
+            //example if the number is out of bounds
+            //Dice dice3 = new Dice(1);
+
+            //intialize variables to assign each dice roll too and to keep track of the number of rolls.
+            int dice1Roll;
+            int dice2Roll;
+            int numberOfRolls = 0;
+            
+            //do while loop to catch when "snake eyes" are rolled (double 1's). 
+            //Also reports the number of rolls. 
+            do
+            {
+                dice1Roll = dice1.rollDice();
+                dice2Roll = dice2.rollDice();
+
+                Console.WriteLine("Rolled: Dice #1 = " + dice1Roll + ", Dice#2 = " + dice2Roll);
+
+                ++numberOfRolls;
+
+            } while (dice1Roll != 1 || dice2Roll != 1);
+
+            //wrting to the console how many number of rolls it took to get snake eyes. 
+            Console.WriteLine("\n" +
+                "It took " + numberOfRolls + " rolls to get snake eyes!");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Dice
+{
+    //intializing private field for the number of sides of the dice
+    private int numberOfSides;
+
+    //constructor that takes in an int number of sides. 
+    //throws exception if the given number is not within the specific range 4 - 20. 
+    public Dice(int numberOfSides)
+    {
+        if(numberOfSides < 4 || numberOfSides > 20)
+        {
+            throw new ArgumentException("Should be within the range 4 - 20 (inclusive)");
+        }
+        this.numberOfSides = numberOfSides;
+    }
+
+    //roll dice method that simulates rolling an n number sided dice using a random number. 
+    public int rollDice()
+    {
+        Random randomRoll = new Random();
+        int roll = randomRoll.Next(1, numberOfSides + 1);
+        return roll;
+    }
+}
